a05facb8b94832e113e40183d05e299b257a299a
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2021-06-20 16:02:18 +0500

    gh-pages

--- src/App.js
+++ src/App.js
@@ -4,15 +4,16 @@ import Navigation from "./components/Navigation/Navigation";
 import ProfileContainer from "./components/Profile/Profile";
 import {BrowserRouter, Redirect, Route, Switch} from "react-router-dom";
 import HeaderContainer from "./components/Header/Header";
-import Messages from "./components/Messages/Messages";
-import UsersContainer from "./components/Users/UsersContainer";
 import NotFound from "./components/NotFound/NotFound";
 import Login from "./components/Login/Login";
 import {connect, Provider} from "react-redux";
-import React from "react";
+import React, {Suspense} from "react";
 import {initializeApp} from "./redux/appReducer";
 import Preloader from "./components/Common/Preloader/Preloader";
 import store from "./redux/redux-store";
+
+const Messages = React.lazy(() => import("./components/Messages/Messages"));
+const UsersContainer = React.lazy(() => import("./components/Users/UsersContainer"));
 
 class App extends React.Component {
     componentDidMount() {
@@ -35,52 +36,60 @@ class App extends React.Component {
                         <div className={classes.body}>
                             <Navigation id={this.props.id}/>
                             <div className={classes.content}>
-                                <Switch>
-                                    <Route path={"/profile/:userId?"} render={() => <ProfileContainer/>}/>
-                                    <Route exact path={"/profile"}>
-                                        <Redirect to={"/login"}/>
-                                    </Route>
-                                    <Route path={"/profile/null"}>
-                                        <Redirect to={"/login"}/>
-                                    </Route>
-                                    <Route path={"/messages"} render={() => <Messages/>}/>
-                                    <Route exact path={"/users"} render={() => <UsersContainer/>}/>
-                                    <Route path={"/404"} render={() => <NotFound/>}/>
-                                    {/*<Route path={"/music/"} render={() => <Music/>}/>*/}
+                                <Suspense fallback={<Preloader/>}>
+                                    <Switch>
+                                        <Route path={"/profile/:userId?"} render={() => <ProfileContainer/>}/>
+                                        <Route exact path={"/profile"}>
+                                            <Redirect to={"/login"}/>
+                                        </Route>
+                                        <Route path={"/profile/null"}>
+                                            <Redirect to={"/login"}/>
+                                        </Route>
+                                        <Route path={"/messages"} render={() => <Messages/>}/>
+                                        <Route exact path={"/users"} render={() => <UsersContainer/>}/>
+                                        <Route path={"/404"} render={() => <NotFound/>}/>
+                                        {/*<Route path={"/music/"} render={() => <Music/>}/>*/}
 
-                                    <Route path={"/"}>
-                                        <Redirect to={"/404"}/>
-                                    </Route>
-                                </Switch>
+                                        <Route path={"/"}>
+                                            <Redirect to={"/404"}/>
+                                        </Route>
+                                    </Switch>
+                                </Suspense>
                             </div>
                         </div>
                         <Footer/>
                     </div>
                 </Route>
             </Switch>
-        );
+    );
     }
-}
-
-let mapStateToProps = (state) => {
-    return {
-        initialized: state.app.initialized,
-        theme: state.app.theme,
-        isAuth: state.auth.isAuth,
-        email: state.auth.email,
-        login: state.auth.login,
-        id: state.auth.id,
     }
-}
 
-const AppContainer = connect(mapStateToProps, {initializeApp})(App);
-const ReactApp = () => {
-    return (
-        <BrowserRouter>
-            <Provider store={store}>
-                <AppContainer/>
-            </Provider>
-        </BrowserRouter>
-    )
-}
-export default ReactApp;
+    let mapStateToProps = (state) =>
+        {
+            return {
+                initialized: state.app.initialized,
+                theme: state.app.theme,
+                isAuth: state.auth.isAuth,
+                email: state.auth.email,
+                login: state.auth.login,
+                id: state.auth.id,
+            }
+        }
+
+    const AppContainer = connect(mapStateToProps,
+        {
+            initializeApp
+        }
+    )(App);
+    const ReactApp = () =>
+        {
+            return (
+                <BrowserRouter>
+                    <Provider store={store}>
+                        <AppContainer/>
+                    </Provider>
+                </BrowserRouter>
+            )
+        }
+    export default ReactApp;
