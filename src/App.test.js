acce875e297e8f8325d62f4d65d948762dc790c5
VsALT2000 <58140251+VsALT2000@users.noreply.github.com>
2021-06-20 16:42:46 +0500

    Updates

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import App from "./App";
-import ReactDOM from "react-dom";
-
-it('render without crashing', function () {
-    const div = document.createElement('div');
-    ReactDOM.render(<App/>, div);
-    ReactDOM.unmountComponentAtNode(div);
-});
--- src/utils/validators/validators.js
+++
@@ -1,5 +0,0 @@
-export const requiredField = (value) => value ? undefined : "Field is required"
-
-export const maxLengthTC = (maxLength) => (value) => {
-    return value && value.length > maxLength ? `Max length is ${maxLength} symbols` : undefined;
-}
